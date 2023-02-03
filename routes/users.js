36e15705eaf0a58417f952744839b461add16e9c
DilanAtomica <dilan-shwane@hotmail.com>
2023-02-03 17:29:16 +0100

    made the authentication with JWT and cookies

--- routes/users.js
+++ routes/users.js
@@ -20,8 +20,8 @@ router.post("/registerUser", async(req, res) => {
         const {email, username, password} = req.body;
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const [usernames] = await pool.query("SELECT username FROM user WHERE username = ?", [req.body.username]);
-        const [emails] = await pool.query("SELECT email FROM user WHERE email = ?", [req.body.email]);
+        const [usernames] = await pool.query("SELECT username FROM user WHERE username = ? LIMIT 1", [req.body.username]);
+        const [emails] = await pool.query("SELECT email FROM user WHERE email = ? LIMIT 1", [req.body.email]);
 
         let takenInputs = {
             username: false,
@@ -30,9 +30,6 @@ router.post("/registerUser", async(req, res) => {
 
         if(usernames.length !== 0) takenInputs = {...takenInputs, username: true}
         if(emails.length !== 0) takenInputs = {...takenInputs, email: true}
-
-        console.log(takenInputs.username);
-        console.log(takenInputs.email);
 
             if (takenInputs.username || takenInputs.email) {
                 res.status(409).json(
@@ -53,35 +50,42 @@ router.post("/registerUser", async(req, res) => {
 });
 
 router.post("/login", async(req, res) => {
-    const {email, password} = req.body;
-    //const user = await Users.findOne({where: { username: username}});
-    //if(!user) res.status(400).json({error: "User does not exist"});
+    try {
+        const {email, password} = req.body;
+        const [user] = await pool.query("SELECT * FROM user WHERE email = ?", [email]);
+        console.log(user[0].password);
+        if(user.length === 0) res.status(404).json({message: "A user with that email does not exist"});
+        else {
+            console.log("here");
+            const matching = await bcrypt.compare(password, user[0].password);
+            if(!matching) res.status(403).json({message: "Wrong Email or password combination"});
+            else {
+                const accessToken = sign({
+                    id: user[0].userID,
+                    email: user[0].email,
+                    username: user[0].username
+                }, "jwtsecretplschange");
 
-    /*
-    const matching = await bcrypt.compare(password, user.password);
-    if(!matching) {
-        res.status(400).json({error: "Wrong username and password combination"});
-    } else {
-        const accessToken = sign({username: user.username, id: user.id}, "jwtsecretplschange");
-
-        res.cookie("accessToken", accessToken, {
-            maxAge: 60*60*24*30*1000,
-            httpOnly: true,
-        }).status(200).json("Logged in Successfully!");
+                res.cookie("accessToken", accessToken, {
+                    maxAge: 60*60*24*30*1000,
+                    httpOnly: true,
+                }).status(200).json({message: "Logged in Successfully!"});
+            }
+        }
+    } catch (error) {
+        res.status(400).json({error: error});
     }
 
-     */
 });
 
 router.post("/auth", validateToken, async(req, res) => {
     if(req.authenticated) {
-        //const user = await Users.findOne({where: { username: req.tokenData.username}});
-        //const userData = {id: user.id, username: user.username, admin:  user.admin};
-        //res.json(userData);
+        const [user] = await pool.query("SELECT * FROM user WHERE email = ?", [req.tokenData.email]);
+        const userData = {id: user.id, email: user.email, username: user.username};
+        res.status(200).json(userData);
     } else {
         res.json(null)
     }
 });
-
 
 module.exports = router;
